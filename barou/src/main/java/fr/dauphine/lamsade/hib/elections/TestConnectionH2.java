93db93671dd8caee4945de5f78370e6052c4b8f6
barou <barou2000@hotmail.com>
2016-03-09 21:33:59 +0100

    Add logger

--- barou/src/main/java/fr/dauphine/lamsade/hib/elections/TestConnectionH2.java
+++ barou/src/main/java/fr/dauphine/lamsade/hib/elections/TestConnectionH2.java
@@ -1,31 +1,30 @@
 package fr.dauphine.lamsade.hib.elections;
 
-import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Logger;
 
 public class TestConnectionH2 {
 	private final String URL = "jdbc:h2:localhost/test";
+	
+	private static final Logger logger=Logger.getLogger(TestConnectionH2.class.getCanonicalName());
 
 	public TestConnectionH2() {
 		try {
+			logger.info("Demarrage");
 			Driver driver = DriverManager.getDriver(URL);
-			System.out.println("H2 pilote JDBC version: "
+			logger.info("H2 pilote JDBC version: "
 					+ driver.getMajorVersion() + "."
 					+ driver.getMinorVersion());
 		} catch (SQLException e) {
-			e.printStackTrace();
+			logger.severe(e.getMessage());
 		}
 	}
 
 	public static void main(String[] args) {
 		new TestConnectionH2();
 
-	}
-
-	private Connection getH2Connection() throws SQLException {
-		return DriverManager.getConnection(URL, "sa", "");
 	}
 
 }
