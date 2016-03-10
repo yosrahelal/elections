e079e6ecc7171d5b0d08cab110954c120c470062
asus <asus@asus-PC>
2016-03-10 11:15:34 +0100

    Test Connection H2

---
+++ src/main/java/org/dauphine/JDBC/JDBC/App.java
@@ -0,0 +1,27 @@
+package org.dauphine.JDBC.JDBC;
+
+import java.sql.*;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class App {
+	public static void main(String[] args) {
+
+		Driver driver = null;
+
+		try {
+
+			driver = (Driver) DriverManager.getDriver("jdbc:h2:~/test");
+
+			System.out.println(
+					"driver version number = " + " " + driver.getMajorVersion() + "." + driver.getMinorVersion());
+
+		} catch (SQLException e) {
+
+			System.out.println("Connection Failed! Check output console");
+			e.printStackTrace();
+			return;
+
+		}
+	}
+}
